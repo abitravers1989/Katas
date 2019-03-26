75c4502884abfb65d7b3a7e5547e9cd1febb80ca
abitravers1989 <atravers4007@gmail.com>
2019-03-26 22:03:56 +0000

    Add negative prime number functionality

--- sumOfDivided.js
+++ sumOfDivided.js
@@ -1,57 +1,79 @@
-const isPrime = (n) => {
-  if (n !== Math.round(n) || n <= 1) {return false}
+const isPrime = n => {
+  if (n !== Math.round(n) || n <= 1) {
+    return false;
+  }
   let isPrimes = true;
-  for (let i = 2; i <= Math.sqrt(n); i+=1) {
-     if (n % i === 0) {isPrimes = false}
+  for (let i = 2; i <= Math.sqrt(n); i += 1) {
+    if (n % i === 0) {
+      isPrimes = false;
+    }
   }
   return isPrimes;
 };
 
 const sumOfDivided = list => {
-  const savedNums = [];
-  list.forEach((item) => {
-    console.log('----->item',item )
-    if ((typeof item) === 'number') {
-      console.log('----->heress')
-      for (let i = 1; i <= item; i+=1) {
-        console.log('----->i', i)
-      if ((item % i) === 0) {
-        if (isPrime(i) === true) {
-          savedNums.push(i)
+  const primeFactorDividers = [];
+  const deDuplicatedPrimeFactorDividers = [];
+
+  const removeDuplicatePrimeFactors = factorsArray => {
+    factorsArray.forEach(item => {
+      if (!deDuplicatedPrimeFactorDividers.includes(item)) {
+        deDuplicatedPrimeFactorDividers.push(item);
+      }
+    });
+  };
+  const positivePrimeSort = () => {
+    deDuplicatedPrimeFactorDividers.sort((a, b) => a - b);
+  };
+
+  const negativePrimeSort = () => {
+    deDuplicatedPrimeFactorDividers.sort((a, b) => b - a);
+  };
+
+  list.forEach(item => {
+    if (item >= 0) {
+      for (let i = 1; i <= item; i += 1) {
+        if (item % i === 0) {
+          if (isPrime(i) === true) {
+            primeFactorDividers.push(i);
+            removeDuplicatePrimeFactors(primeFactorDividers);
+            positivePrimeSort();
+          }
         }
       }
-     }
     }
-  })
-  const newArray = [];
-  savedNums.forEach(item => {
-    if(!newArray.includes(item)) {
-      newArray.push(item)
+    if (item < 0) {
+      for (let z = 0; z >= item; z -= 1) {
+        if (item % z === 0) {
+          if (isPrime(Math.abs(z)) === true) {
+            primeFactorDividers.push(z);
+            removeDuplicatePrimeFactors(primeFactorDividers);
+            negativePrimeSort();
+          }
+        }
+      }
     }
-  })
-
-  newArray.sort((a, b) => a - b)
-  console.log('----->newArray', newArray)
+  });
   
-const finalArray = [];
-  newArray.forEach(num => {
+  const finalArray = [];
+  deDuplicatedPrimeFactorDividers.forEach(num => {
     const primeFactorsArray = [];
-    list.forEach((listItem) => {
+    list.forEach(listItem => {
       if (listItem % num === 0) {
         if (primeFactorsArray.includes(num)) {
-          primeFactorsArray[1] += listItem
+          primeFactorsArray[1] += listItem;
         } else {
-          primeFactorsArray.push(num, listItem)
+          primeFactorsArray.push(num, listItem);
         }
       }
-    })
-    finalArray.push(primeFactorsArray)
-  })
+    });
+    finalArray.push(primeFactorsArray);
+  });
   return finalArray;
-}
+};
 
+// console.log('----->', sumOfDivided([15,21,24,30,45]))
+// console.log('----->', sumOfDivided([ 107, 158, 204, 100, 118, 123, 126, 110, 116, 100 ]))
 
-//console.log('----->', sumOfDivided([15,21,24,30,45]))
-//console.log('----->', sumOfDivided([ 107, 158, 204, 100, 118, 123, 126, 110, 116, 100 ]))
-
-console.log('----->', sumOfDivided([ -29804, -4209, -28265, -72769, -31744 ]))
+console.log('----->', sumOfDivided([-29804, -4209]));
+// -28265, -72769, -31744
